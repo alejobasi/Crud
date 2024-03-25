29b3dcb72317d8afdff2f590dbac1b7f7aa97cf5
alejobasi <alejo3@educarex.es>
2024-03-25 11:03:48 +0100

    s

---
+++ src/components/CrudApp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const initialDb = [
+  {
+    id: 1,
+    name: "Seiya",
+
+  }
+]
+
+const CrudApp = () => {
+  return (
+    <div>
+      <h2>CRUD APP</h2>
+    </div>
+  );
+}
+
+export default CrudApp;
