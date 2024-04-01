2c089af8cd05c3784eacb3528cf6187c93c16912
Alejo Basilio Alfonso <basilioalfonsoalejo@gmail.com>
2024-04-01 22:53:14 +0200

    sad

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export const useModal = () => {
-  const [isOpen, setIsOpen] = useState(initialValue);
+  const [isOpen, setIsOpen] = useState(false);
 
   const openModal = () => setIsOpen(true);
   const closeModal = () => setIsOpen(false);
