ee50580349a537b1e55249390a3bd995426f915c
alejobasi <alejo3@educarex.es>
2024-04-02 08:50:01 +0200

    v

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,13 +1,16 @@
 import { useModal } from "../hooks/useModal";
-import ContactForm from './ContactForm';
+import ContactForm from "./ContactForm";
 import Modal from "./Modal";
-import SongSearch from './SongSearch';
+import SongSearch from "./SongSearch";
+import ModalPortal from "./ModalPortal";
 
 const Modals = () => {
   const [isOpenModal1, openModal1, closeModal1] = useModal(false);
   const [isOpenModal2, openModal2, closeModal2] = useModal(false);
   const [isOpenContact, openModalContact, closeModalContact] = useModal(false);
   const [isOpenSong, openModalSong, closeModalSong] = useModal(false);
+  const [isOpenPortal, openModalPortal, closeModalPortal] = useModal(false);
+
   return (
     <div>
       <h2>Modales</h2>
@@ -16,7 +19,7 @@ const Modals = () => {
         <h3>Modal 1</h3>
         <p>Hola este es el contenido de mi modal 1</p>
         <img src="foto.jpg" width="400" alt="animals" />
-      </Modal >
+      </Modal>
       <button onClick={openModal2}>Modal 2</button>
       <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
         <h3>Otro Modal</h3>
@@ -32,12 +35,22 @@ const Modals = () => {
       <button onClick={openModalContact}>Modal Contacto</button>
       <Modal isOpen={isOpenContact} closeModal={closeModalContact}>
         <ContactForm />
-      </Modal >
+      </Modal>
 
       <button onClick={openModalSong}>Modal Canciones</button>
       <Modal isOpen={isOpenSong} closeModal={closeModalSong}>
         <SongSearch />
-      </Modal >
+      </Modal>
+
+      <button onClick={openModalPortal}>Modal en Portal</button>
+      <ModalPortal isOpen={isOpenPortal} closeModal={closeModalPortal}>
+        <h3>Modal en Portal</h3>
+        <p>
+          Este es el contenido de un modal que carga en otro modo del DOM
+          diferente a donde carga nuestra app de React, gracias a los Portales
+        </p>
+        <img src="foto.jpg" width="400" alt="animals" />
+      </ModalPortal>
     </div>
   );
 };
